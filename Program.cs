451dc943d7472eb5d5de2c75b9fe282a6922d419
Laisa Andrade <110181258+LaisaCCAndrade@users.noreply.github.com>
2023-12-12 15:08:21 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,95 +1,30 @@
 using System;
-
-// Classe abstrata Smartphone
-public abstract class Smartphone
-{
-    public string Marca { get; set; }
-    public string Modelo { get; set; }
-
-    // Método abstrato que deve ser implementado nas classes filhas
-    public abstract void Ligar();
-    
-    // Método abstrato que deve ser implementado nas classes filhas
-    public abstract void Desligar();
-
-    // Método virtual que pode ser substituído nas classes filhas
-    public virtual void FazerChamada(string numero)
-    {
-        Console.WriteLine($"Realizando chamada para {numero}...");
-    }
-
-    // Método abstrato que deve ser implementado nas classes filhas
-    public abstract void InstalarAplicativo(string aplicativo);
-}
-
-// Classe Nokia que herda de Smartphone
-public class Nokia : Smartphone
-{
-    public Nokia(string modelo)
-    {
-        Marca = "Nokia";
-        Modelo = modelo;
-    }
-
-    // Implementação do método abstrato Ligar
-    public override void Ligar()
-    {
-        Console.WriteLine($"{Marca} {Modelo} ligando...");
-    }
-
-    // Implementação do método abstrato Desligar
-    public override void Desligar()
-    {
-        Console.WriteLine($"{Marca} {Modelo} desligando...");
-    }
-
-    // Implementação do método abstrato InstalarAplicativo
-    public override void InstalarAplicativo(string aplicativo)
-    {
-        Console.WriteLine($"Instalando {aplicativo} na {Marca} {Modelo}...");
-    }
-}
-
-// Classe iPhone que herda de Smartphone
-public class iPhone : Smartphone
-{
-    public iPhone(string modelo)
-    {
-        Marca = "iPhone";
-        Modelo = modelo;
-    }
-
-    // Implementação do método abstrato Ligar
-    public override void Ligar()
-    {
-        Console.WriteLine($"{Marca} {Modelo} ligando...");
-    }
-
-    // Implementação do método abstrato Desligar
-    public override void Desligar()
-    {
-        Console.WriteLine($"{Marca} {Modelo} desligando...");
-    }
-
-    // Implementação do método abstrato InstalarAplicativo
-    public override void InstalarAplicativo(string aplicativo)
-    {
-        Console.WriteLine($"Instalando {aplicativo} no {Marca} {Modelo}...");
-    }
-}
+using DesafioPOO.Models;
 
 class Program
 {
     static void Main()
     {
-        // Exemplo de uso
-        Nokia nokia = new Nokia("Lumia");
-        iPhone iphone = new iPhone("12 Pro");
+        // Criar instância de Nokia
+        Nokia nokia = new Nokia("123456789");
+        nokia.Marca = "Nokia";
+        nokia.Modelo = "Lumia";
+        nokia.TamanhoTela = 5.0m;
 
+        // Testar métodos da classe Nokia
         nokia.Ligar();
+        nokia.ReceberLigacao();
         nokia.InstalarAplicativo("WhatsApp");
 
+        // Criar instância de iPhone
+        Iphone iphone = new Iphone("987654321");
+        iphone.Marca = "iPhone";
+        iphone.Modelo = "12 Pro";
+        iphone.TamanhoTela = 6.1m;
+
+        // Testar métodos da classe iPhone
         iphone.Ligar();
+        iphone.ReceberLigacao();
         iphone.InstalarAplicativo("Instagram");
     }
 }
